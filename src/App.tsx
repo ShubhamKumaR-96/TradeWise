2cfb1700b82052724b4fd41fd68930d5b33368eb
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2026-02-20 12:17:16 +0530

    chore: initial project setup with vite + React + TypeScript +  TailwindCss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#080C14] flex items-center justify-center">
+      <p className="text-emerald-400 font-mono text-xl">
+        TradeWise - Trading Platform
+      </p>
+    </div>
+  );
+}
+
+export default App;
